1d9c08678be38266ae0dc955565316192e1c27da
wangshuai <wangshuai@njjialong.cn>
2021-02-01 09:08:13 +0800

    修改配置文件

--- config.js
+++ config.js
@@ -1,27 +1,8 @@
+
+
 const port = 1883
 
-const list = [
-    /**
-     *  temp
-     * {
-            desc:"",
-            request:"",
-            response:"",
-            body:""
-        },
-     */
-    
-    // 查询版本信息
-    {
-        desc:"查询版本信息返回",
-        request: "app/get/request/database/version",
-        response: "database/get/response/app/version",
-        body: {
-            "version": "2.1",
-            "Compatible": "1.0"
-        }
-    },
-]
+const list = require("./config.json")
 
 module.exports = {
     port,
